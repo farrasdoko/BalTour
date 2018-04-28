76a951c5ceb6f405721b584de7e74ad7160f7ffa
farrasdoko <farras.abiyyu.doko@gmail.com>
2018-04-28 09:44:35 +0700

    Initial commit

---
+++ app/src/main/java/com/gmail/farasabiyyu12/baltour/MainActivity.java
@@ -0,0 +1,93 @@
+package com.gmail.farasabiyyu12.baltour;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recy;
+    CustomAdapter adap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO Recycler View Method
+        recy = findViewById(R.id.recycler_view);
+        recy.setLayoutManager(new LinearLayoutManager(this));
+
+        //TODO Set Adapter
+        adap = new CustomAdapter();
+        recy.setAdapter(adap);
+    }
+
+    String[] judul = {"Tanah Lot", "Pantai Kuta", "Pantai Sanur", "Beachwalk", "Monumen Bajra Sandhi",
+            "Museum Bali",
+//            "Pantai Double Six", "Pura Besakih", "Pura Tirta Empul", "Gunung Agung"
+    };
+    int[] gambar = {
+            R.drawable.tanahlot,
+            R.drawable.pantaikuta,
+            R.drawable.pantaisanur,
+            R.drawable.beachwalk,
+            R.drawable.monumenbajrasandhi,
+            R.drawable.museumbali,
+//            R.drawable.pantaidoublesix,
+//            R.drawable.purabesakih,
+//            R.drawable.pura_tirta_empul,
+//            R.drawable.gunung_agung
+
+
+    };
+
+    private class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.listview, null);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, final int position) {
+            holder.judul.setText(judul[position]);
+            holder.img.setImageResource(gambar[position]);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getApplicationContext(), ScrollingActivity.class)
+                    .putExtra("judul", judul[position])
+                    .putExtra("gambar", gambar[position]));
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return judul.length;
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+
+            TextView judul;
+            ImageView img;
+
+            public MyViewHolder (View itemView){
+                super(itemView);
+
+                judul = itemView.findViewById(R.id.txtjudul);
+                img = itemView.findViewById(R.id.gambar);
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/gmail/farasabiyyu12/baltour/ScrollingActivity.java
@@ -0,0 +1,67 @@
+package com.gmail.farasabiyyu12.baltour;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    TextView txtDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //TODO Intent
+        Intent intent = getIntent();
+
+        String strjudul = intent.getStringExtra("judul");
+        int strgambar = intent.getIntExtra("gambar", 0);
+        //TODO Setting Toolbar
+        toolbar.setTitle(strjudul);
+
+        try {
+            Glide.with(this).load(strgambar).into((ImageView) findViewById(R.id.gambar));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //TODO setDetail Text
+        txtDetail = findViewById(R.id.txtdetail);
+
+        if (strjudul.equals("Tanah Lot")){
+            txtDetail.setText(R.string.tanah_lot);
+        }else if (strjudul.equals("Pantai Kuta")){
+            txtDetail.setText(R.string.pantai_kuta);
+        }else if (strjudul.equals("Pantai Sanur")){
+            txtDetail.setText(R.string.pantai_sanur);
+        }else if (strjudul.equals("Beachwalk")){
+            txtDetail.setText(R.string.beachwalk);
+        }else if (strjudul.equals("Monumen Bajra Sandhi")){
+            txtDetail.setText(R.string.bajra_sandhi);
+        }else if (strjudul.equals("Museum Bali")) {
+            txtDetail.setText(R.string.museum_bali);
+        }else if (strjudul.equals("Pantai Double Six")){
+            txtDetail.setText(R.string.pantai_double_six);
+        }else if (strjudul.equals("Pura Besakih")){
+            txtDetail.setText(R.string.pura_besakih);
+        }else if (strjudul.equals("Pura Tirta Empul")) {
+            txtDetail.setText(R.string.pura_tirta_empul);
+        }else if (strjudul.equals("Gunung Agung")){
+            txtDetail.setText(R.string.gunung_agung);
+        }
+    }
+}
